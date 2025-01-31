23c6dc20700a7125dd5c57cc0de542707a992199
dazedRbot <jacoblimpin3@gmail.com>
2025-01-31 19:07:32 +0800

    Update login.php

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             
             header("Location: index.php");
             exit;
-        }
+        } //hello
     }
     
     $is_invalid = true;
